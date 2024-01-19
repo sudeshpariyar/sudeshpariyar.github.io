dfb1f1a685cc3b734dcc1a039859e63121cd8484
sudeshpariyar <sudesh.bullet@gmail.com>
2024-01-19 16:55:20 +0800

    adding background pic

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import "./app.css";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import ContentWrapper from "./shared/content-wrapper/ContentWrapper";
+import background from "./asset/background.webp";
 
 const App = () => {
   return (
     <div className="app-wrapper">
       <Header />
-      <div className="app-body-wrapper">
+      <div
+        className="app-body-wrapper"
+        style={{
+          backgroundImage: `url(${background})`,
+        }}
+      >
         <ContentWrapper>
           <div className="launching-wrapper">Launching Soon</div>
         </ContentWrapper>
