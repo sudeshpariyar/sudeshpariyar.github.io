1c38802c0328daf8a1f25dbb1ae5b82c2edca710
sudeshpariyar <sudesh.bullet@gmail.com>
2024-01-22 14:56:56 +0800

    This is the test branch

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => {
         }}
       >
         <ContentWrapper>
-          <div className="launching-wrapper">Launching Soon </div>
+          <div className="launching-wrapper">Launching Soon test branch</div>
         </ContentWrapper>
       </div>
       <Footer />
