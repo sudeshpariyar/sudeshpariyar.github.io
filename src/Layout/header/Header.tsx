ed528e61d9ab65d4a2a146d8b29b426ae71e6ea0
sudeshpariyar <sudesh.bullet@gmail.com>
2024-04-10 16:00:58 +1000

    added responsiveness to the dwebsite

--- src/Layout/header/Header.tsx
+++ src/Layout/header/Header.tsx
@@ -1,31 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
 import ContentWrapper from "../../components/content-wrapper/ContentWrapper";
 import CustomButton from "../../components/CustomButton/CustomButton";
-import { Link } from "react-router-dom";
-import logo from "../../asset/full-logo.png";
+import { Link, useNavigate } from "react-router-dom";
+import { IoCloseSharp } from "react-icons/io5";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+import CustomHeading from "../../components/CustomHeading/CustomHeading";
 
 const Header = () => {
+  const [mobileNavClicked, setMobileNavClicked] = useState(false);
+
+  const navigate = useNavigate();
+  const handleNav = () => {
+    setMobileNavClicked(!mobileNavClicked);
+  };
+  const handleLinkClicked = (link: string) => {
+    navigate(`${link}`);
+  };
   return (
     <div className="header__wrapper">
       <ContentWrapper>
         <div className="header__items">
           <Link to="/">
-            <img className="header__logo" src={logo} alt="logo" />
+            <CustomHeading headingLevel="h1" className="heading__white">
+              Suhara
+            </CustomHeading>
           </Link>
-          <div className="header__navItems">
-            <CustomButton className="header__button">
-              <Link to="/">Home</Link>
+
+          <div
+            className={
+              mobileNavClicked ? "header__navItems" : "header__navItems__none"
+            }
+          >
+            <CustomButton
+              className="header__button"
+              onClick={() => handleLinkClicked("/")}
+            >
+              Home
             </CustomButton>
-            <CustomButton className="header__button">
-              <Link to="/recruitment">Recruitments</Link>
+            <CustomButton
+              className="header__button"
+              onClick={() => handleLinkClicked("/recruitment")}
+            >
+              Recruitments
             </CustomButton>
             <CustomButton className="header__button">Clients</CustomButton>
-            <CustomButton className="header__button">
-              <Link to="/about">About</Link>
+            <CustomButton
+              className="header__button"
+              onClick={() => handleLinkClicked("/about")}
+            >
+              About
             </CustomButton>
             <CustomButton className="header__button">Contacts</CustomButton>
           </div>
+          <CustomButton onClick={handleNav} className="header__mobile__nav">
+            {mobileNavClicked ? <IoCloseSharp /> : <GiHamburgerMenu />}
+          </CustomButton>
         </div>
       </ContentWrapper>
     </div>
